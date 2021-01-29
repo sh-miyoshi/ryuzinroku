b178bb35a6530f6f43fbcf0bdbdf4eb951963c4f
s.miyoshi <s.miyoshi1991@gmail.com>
2021-01-29 10:10:01 +0900

    update effect cotroller

--- pkg/effect/controllerAct.go
+++ pkg/effect/controllerAct.go
@@ -2,6 +2,7 @@ package effect
 
 import (
 	"math"
+	"math/rand"
 
 	"github.com/sh-miyoshi/dxlib"
 	"github.com/sh-miyoshi/ryuzinroku/pkg/background"
@@ -13,11 +14,16 @@ import (
 func ctrlAct0(c *Controller) bool {
 	if c.count%2 == 0 {
 		// エフェクトの登録
+		col := c.Color
+		if col < 0 {
+			col = rand.Intn(5)
+		}
+
 		e := effect{
 			Type:       effectDead,
 			BlendType:  dxlib.DX_BLENDMODE_ADD,
 			BlendParam: 255,
-			Color:      c.Color,
+			Color:      col,
 			X:          c.X,
 			Y:          c.Y,
 			ExtRate:    0.5,
